b368db412fec43eea2f020b733a9cbf34d586e0f
Nick Lewis <nlewis@bullhorn.com>
2019-05-08 15:48:16 -0400

    Add SplEnum

---
+++ Library/Bullhorn/FastRestServices/SplEnum.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bullhorn\FastRestServices;
+//Fix for default SplEnum not being available in windows
+abstract class SplEnum implements \JsonSerializable {
+    /** @var  string */
+    private $value;
+
+    final public function __construct($value) {
+        $c = new \ReflectionClass($this);
+        if(!in_array($value, $c->getConstants())) {
+            throw new \InvalidArgumentException('Invalid Value: '.$value);
+        }
+        $this->value = $value;
+    }
+
+    /**
+     * getValues
+     * @return array
+     */
+    public static function getValues() {
+        $c = new \ReflectionClass(static::class);
+        return $c->getConstants();
+    }
+
+    final public function __toString() {
+        return $this->value;
+    }
+
+    public function jsonSerialize() {
+        return $this->__toString();
+    }
+
+
+}
