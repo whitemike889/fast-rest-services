3d503be6d55380bf792c0cd3af68692cd96068f7
Nick Lewis <nlewis@bullhorn.com>
2019-05-09 16:54:53 -0400

    Add data validations

---
+++ Library/Bullhorn/FastRestServices/Date/DateTime.php
@@ -0,0 +1,24 @@
+<?php
+namespace Bullhorn\FastRestServices\Date;
+class DateTime extends Date {
+    /**
+     * Converts to a date time format
+     * @return string
+     */
+    public function __toString() {
+        return $this->toDateTime();
+    }
+
+    /**
+     * format
+     * @param string|null $format
+     * @return bool|string
+     */
+    public function format($format = null) {
+        if(is_null($format)) {
+            $format = $this->getFormatter()->getDateTimeFormat();
+        }
+        return date($format, $this->getEpoch());
+    }
+
+}
